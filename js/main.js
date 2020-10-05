c72db6398882f8d02374d479f523ea5e4c7482e7
robin-hood88 <64959320+robin-hood88@users.noreply.github.com>
2020-10-05 17:21:30 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-Console.log('');
+let articles = document.querySelectorAll('.featured-posts__item');
+let bigArticle = document.querySelectorAll('.featured-posts__big-item');
+let filter = document.querySelector('.filter');
+let comment = document.getElementById('textarea');
+let submitButton = document.querySelector('.btn-submit');
+  
+  filter.onchange = function () {
+    for (let article of articles) {
+        if (article.dataset.category !== filter.value && filter.value !== 'all') {
+          article.classList.add('hidden');
+       } else {
+        article.classList.remove('hidden');
+       }
+      }
+    for (let article of bigArticle) {
+      if (article.dataset.category !== filter.value && filter.value !== 'all') {
+        article.classList.add('hidden');
+     } else {
+        article.classList.remove('hidden');
+     }
+    }
+  };
+
+  comment.oninput = function () {
+    if (comment.value.length > 200 || comment.value.length < 10) {
+        comment.classList.add('warning');
+        submitButton.disabled = true;
+        submitButton.style.opacity = '.5';
+        submitButton.style.cursor = 'not-allowed';
+    } else {
+        comment.classList.remove('warning');
+        submitButton.disabled = false;
+        submitButton.style.opacity = '1';
+        submitButton.style.cursor = 'pointer';
+    }
+  };
+
+
+  
+  
